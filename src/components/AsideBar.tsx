2cef34e9f5e2e1726b2e6c019923b27b5877d9b4
JotaErrE01 <jruiz967@gmail.com>
2022-03-19 20:55:12 -0500

    Modificando el sideBar

--- src/components/AsideBar.tsx
+++ src/components/AsideBar.tsx
@@ -20,7 +20,7 @@ export const AsideBar = () => {
       <ul className="list-none text-white lg:text-xl text-base md:text-lg lg:h-4/5 my-auto flex lg:block flex-wrap font-bold justify-between h-full flex-1">
         {
           Object.keys(optionsList).map((optionList) => {
-            if (urList?.includes(optionList)) {
+            if (urList?.includes(optionList)) {              
               return optionsList[(optionList as keyof typeof optionsList)].map(({ id, to, title }, i) => (
                 <li key={id} className="my-2 lg:my-8 lg:ml-2 block lg:p-0 text-center lg:text-left w-full">
                     <NavLink
@@ -29,7 +29,7 @@ export const AsideBar = () => {
                         borderBottom: isActive ? '2px solid rgb(59 130 246 / 1)' : '',
                       })}
                       className="hover:border-b-2 border-b-blue-500 hover:cursor-pointer"
-                    >{title}</NavLink>
+                    >{urList?.includes('inicio') && `Unidad ${i + 1} -` }{title}</NavLink>
                 </li>
               ))
             }
